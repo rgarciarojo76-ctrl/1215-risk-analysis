b13e1e285e47bc7035a80dede4ac77d63d82c9dd
Ruben Garcia Rojo <rubengarciarojo@Mac-mini-de-Ruben.local>
2026-01-13 14:30:03 +0100

    Security: Implement Gatekeeper for Portal Integration

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import { useState } from 'react'
 import DashboardLayout from './layouts/DashboardLayout'
 import './App.css'
 
+import Gatekeeper from './components/Gatekeeper';
+
 function App() {
     return (
-        <DashboardLayout />
+        <Gatekeeper>
+            <DashboardLayout />
+        </Gatekeeper>
     )
 }
 
